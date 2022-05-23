9b7729f8442b5f219dc6dd01db285dad5073c7ce
Master <ehtiramabdullayev@gmail.com>
2022-05-23 20:37:36 +0200

    Update PaperStrategy.java

--- src/main/java/org/example/strategy/impl/PaperStrategy.java
+++ src/main/java/org/example/strategy/impl/PaperStrategy.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.example.strategy.GameStrategy;
 import org.example.types.ResultType;
-
 import static org.example.types.Constants.*;
 
 public class PaperStrategy implements GameStrategy {
